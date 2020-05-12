104638c1108d3cd818af52c5f71cd1d558c93380
tongfy <583682532@qq.com>
2020-05-12 16:12:04 +0800

    add TTBaseTableViewController

---
+++ TTToolKit/Base/TTBaseTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  TTBaseTableViewController.h
+//  CVoice2
+//
+//  Created by Tong on 2020/4/28.
+//  Copyright © 2020 CChip. All rights reserved.
+//
+
+#import "TTBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTBaseTableViewController : TTBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
